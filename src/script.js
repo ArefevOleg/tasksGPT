edce90c9fa3c0e64bccf952848992c80927a5e33
Arefevpro <arefevmsk@gmail.com>
2024-04-30 19:08:46 +0300

    ./

---
+++ src/script.js
@@ -0,0 +1,288 @@
+// Создайте функцию, которая принимает целое число в
+// качестве аргумента и возвращает "Четное" для четных чисел или "Нечетное"
+// для нечетных чисел.
+
+// Function declaration
+
+function calculateNumber(initialValue) {
+  // initialValue - параметр функции типа number можно передать колбэк
+  let x = initialValue;
+  x = x + 100;
+  x = x / 5;
+
+  return x;
+}
+
+const result = calculateNumber(5334535);
+const result1 = calculateNumber(5335);
+// console.log(result);
+// console.log(result1);
+
+function calculateCercleArea(cercleRadius) {
+  // initialValue - параметр функции типа number
+  return 3.14 * cercleRadius ** 2; // параметр - это переменная
+} // функция может принимать сколько угодно параметров
+
+const cercleArea1 = calculateCercleArea(6); // (6) - это аргумент (значение переменной, которе мы передаем в функцию)
+// console.log(cercleArea1);
+
+const cercleArea2 = calculateCercleArea(16.2);
+// console.log(cercleArea2);
+
+function createHTMLElement(tag = "button", text = "отправить", id = null) {
+  // нужно сохранять порядок записи параметров
+  const element = document.createElement(tag);
+  if (id) {
+    // если id не null или undefined то присваиваем элементу id
+    element.id = id;
+  }
+  element.innerHTML = text;
+
+  return element;
+}
+
+const h1element = createHTMLElement("h1", "просто текст"); // аргументы идут в такомже порядке как и параметры
+// console.log(h1element)                                  // если мы не хотим передавать аргументы, то мы можем использовать null
+// document.body.append(h1element)
+
+// const element = createHTMLElement();
+// document.body.append(element)
+
+function logArgsToControle() {
+  // console.log(arguments);
+  if (!arguments.length) {
+    //1. если аргументов нет
+    return; // то досрочно возвращаем завршение функции
+  }
+  for (let argument of arguments) {
+    // console.log(arguments);
+  }
+  console.log("Больше аргументов нет"); // когда аргументы закончились выводим это сообщение
+}
+
+logArgsToControle(); // 1. если аргументов нет то возвращаем undefined
+
+// Function expression
+
+// sayHello(); - нельзя получить доступ к переменной до т ого как она была инициализированна
+
+// const sayHello = function() {
+//   console.log('Hello');
+// }
+// sayHello();
+
+const sayBye = () => {
+  // у стрелонной функции нет параметра аргументов
+  // console.log('Bye');
+};
+sayBye();
+
+const brokenLinks = ["vk", "youtybe", "instagram", "facebook"];
+// https://vk.com
+
+// коллбэк функция - это функция переданная в другую функцию в качестве аргумента
+const fixLinksArray = (linksArray) => {
+  const correctLinks = [];
+  linksArray.forEach((brokenLink) =>
+    correctLinks.push("https://" + brokenLink + ".com")
+  );
+
+  return correctLinks;
+};
+
+// console.log(fixLinksArray(brokenLinks));
+
+// методы массивов
+
+const people = [
+  { name: "Олег", age: 15, budget: 20000 },
+  { name: "Алексей", age: 15, budget: 3400 },
+  { name: "Михаил", age: 15, budget: 5600 },
+  { name: "Екатерина", age: 15, budget: 7000 },
+  { name: "Вадим", age: 15, budget: 4500 },
+  { name: "Елена", age: 15, budget: 8200 },
+];
+
+// ForEach
+
+// Map
+
+// Filter
+
+// Reduce
+
+// Find
+
+// FindIndex
+
+// Циклы FOR
+
+// Цикл - это что-то замкнутое и повторяющееся действие из раза в раз
+
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+
+//     or
+
+// 1. создаем переменную - счётчик i (индекс) = 0
+// 2. указываем условие (i < 5) меньшели переменная i чем 5
+// 3. переходим к телу цикла и выводим через console.log(1)
+// 4. шаг (увеличиваем i на 1) Это эквивалентно операции i = i + 1
+
+// for (1.начальное значение;2. условие; 4.) {
+// 3.  console.log(1);
+
+// создаем переменную - счётчик i и приравниваем ее к 0
+// проверяем меньше ли i чем 5? получаем true
+// выполняем console.log(1)
+// } i++ прибавляем единицу к i
+// снова проверяем меньше ли i чем 5? получаем true
+// и так далее до тех пор пока i будет равна 5 и получим false тогда тело цикла не будет выполненно
+
+for (let i = 0; i < 10; i++) {
+  // console.log(i);   // для проверки можем выводить i в место 1
+}
+
+const arr = ["a", "b", "c", "d", "e"];
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// модификация записи цикла FOR
+
+// в letter находится следующий элемент массива a b c d e
+// переменную letter можно назвать как угодно, например - eretete
+for (let letter of arr) {
+  // console.log(letter);
+}
+
+const user = {
+  name: "Олег",
+  age: "15",
+  isMaried: true,
+};
+
+for (let key in user) {
+  // console.log(key); // получаем доступ к каждому ключу в объекте
+  // console.log(key, user[key]);  // получаем свойство объекта
+  // пишем [key] в квадратных скобках потомучто значение [key] динамическое
+  if (key === "age") {
+    break; // останавливает цикл
+  }
+  // console.log(user[key]);
+}
+
+//  в обратном порядке сначала i = 5, потом i = 4 и так далее
+// for (let i = arr.length; i > 0; i--) {
+// console.log(arr[i - 1]);
+// }
+
+// Циклы WHILE
+
+let x = 0;
+
+// while (x < 5) {
+// console.log(x);
+// x++;
+// break; // останавливает цикл
+// }
+
+// let word = '';
+// while (word.length < 5) {
+// const value = prompt('введите букву');
+//   if (value.length === 0) { // если пользователь ничего не ввел то выходим  из цикла
+//     break;
+//   }
+//   word = word + value;
+//   console.log(word);
+// }
+
+// сначала делаем, а потом проверяем
+
+// let h = 0;
+// do {
+//   console.log(h);
+//   h++;
+// } while (h < 5);
+
+// // сначала проверяем, а потом делаем
+
+// let y = 0;
+// while (y < 5) {
+//   console.log(y);
+//   y++;
+// }
+
+// const sum = (a, b) => {
+//   return a + b;
+// };
+// const res = sum(sum(1, 2), sum(1, 3));
+// // const res1 = sum(2, 4);
+
+// console.log(res );
+
+// "StartsWith" - это метод, используемый в JavaScript для проверки, начинается ли строка с определенной подстроки. Этот метод возвращает логическое значение true, если строка начинается с указанной подстроки, и false в противном случае.
+
+const languages = ["Java", "TypeScript", "C#", "JavaScript", "Julia"]
+
+languages.filter(function (item, index, arr) {
+  // console.log("Текущий элемент " + item)
+  // console.log("Индекс " + index)
+  // console.log("Массив " + arr)
+
+  return index >= 3
+})
+
+
+// const jLanguages = languages.filter(function (language) {
+//   return language.startsWith("T")
+// })
+// // Результат будет ['Java', 'JavaScript', 'Julia'] 
+
+// console.log(jLanguages)
+
+
+
+const arr1 = [1,2,3,4,5];
+
+// 1. forEach - перебор элементов массива 
+// item - элемент массива
+//      [1,2,3,4,5]
+// imdex 0,1,2,3,4
+// const newArr = arr1.forEach((item, imdex, array) => {
+// console.log(item, imdex, array)
+// });
+// console.log(newArr) - undefined
+
+// 2. map - перебор элементов массива и возврат нового массива на основе текущего
+// const newArr = arr1.map((item, index, array) => {
+// return item * 2 + index;
+// });
+
+// console.log(newArr)
+// 3. filter - возвращает новый массив 
+// позволяет получить новый массив, отфильтровав элементы с помощью переданной колбэк-функции. Колбэк-функция будет вызвана для каждого элемента массива и по результату функции примет решение включать этот элемент в новый массив или нет.
+
+// const filterArr = arr1.filter((item, imdex, array) => {
+// return item > 3;
+// });
+// console.log(filterArr)
+
+const array = [1,2,3,4,5]
+
+const newArray = array.map(function(element) {
+  return element + '0'
+})
+
+
+
+console.log(newArray)
