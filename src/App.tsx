b56d8da5051982ab0df5577192bbe4189b7c455f
Arefevpro <arefevmsk@gmail.com>
2024-04-30 09:55:13 +0300

    ./

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
-
-
+import './array/solutionArray'
 
 function App() {
   return (
--- src/array/SolutionArray.js
+++ src/array/SolutionArray.js
@@ -1,2 +1,9 @@
 
-  
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const squares = nums.map(function (num) {
+  return num * num
+})
+
+console.log(squares)
+// [1, 4, 9, 16, 25, 36, 49, 64, 81]
