04e678c7b72982d2b22ef8fcccaaf2d27b81f325
Arefevpro <arefevmsk@gmail.com>
2024-05-09 18:55:16 +0300

    ./

--- src/basicsJS/theBasicsJS.js
+++ src/basicsJS/theBasicsJS.js
@@ -17,4 +17,46 @@ return a + b
 }
 // Вызов функции sum
 let result = sum(1,2)
-console.log(result );
+// console.log(result );
+// 6.
+// function greet(nameOne) {
+//   return "Hello, " + nameOne + "!";
+// }
+
+// console.log(greet("Anna"));
+const greet = (nameOne) => {
+  return "Hello, " + nameOne + "!";
+}
+let nameOne = greet("Anna")
+// console.log(nameOne );
+// 7.
+function calculateArea(width, height) {
+  return width * height; // Возвращаем произведение ширины на высоту
+}
+
+let rectangleArea = calculateArea(10,12) // Вычисляем площадь прямоугольника
+// console.log(rectangleArea);
+// 8.
+function isPrime(number) {
+  if (number <= 1) {
+    return false; // Число меньше или равно 1 не является простым
+  }
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false; // Если число делится без остатка на i, то оно не простое
+    }
+  }
+
+  return true; // Если не найдены делители, то число простое
+}
+
+// Теперь можно вызвать функцию isPrime с различными аргументами
+// console.log(isPrime(7)); // Выведет true, так как 7 - простое число
+// console.log(isPrime(10)); // Выведет false, так как 10 - не простое число
+// console.log(isPrime(1)); // Выведет false, так как 1 не является простым числом
+// console.log(isPrime(17)); // Выведет true, так как 17 - простое число
+// 9.
+function countVowels() {
+  
+}
--- src/map/map.js
+++ src/map/map.js
@@ -1,61 +1,42 @@
-// Пример исходного массива
+// 1. 
 const numbers = [1, 2, 3, 4, 5];
-
-// Применяем метод map для удвоения каждого элемента
-const doubledNumbers = numbers.map((num) => {
-    return num * 2;
-});
-
-//console.log(doubledNumbers); // Вывод: [2, 4, 6, 8, 10]
-
-const persons = [
-  { name: 'Alice', age: 25 },
-  { name: 'Bob', age: 30 },
-  { name: 'Charlie', age: 35 }
-];
-
-// Используем map для получения массива имен
-const names = persons.map((person) => {
-  return person.name;
-});
-
-//console.log(names); // Вывод: ['Alice', 'Bob', 'Charlie']
-
-// const products = [
-//   { id: 1, name: 'Телефон', price: 500, quantity: 3 },
-//   { id: 2, name: 'Ноутбук', price: 1200, quantity: 0 },
-//   { id: 3, name: 'Планшет', price: 300, quantity: 5 }
-// ];
-
-// // Используем map для создания нового массива с расширенной информацией о продуктах
-// const updatedProducts = products.map((product) => {
-//   return {
-//       ...product,
-//       status: product.quantity > 0 ? 'в наличии' : 'нет в наличии'
-//   };
-// });
-
-// console.log(updatedProducts);
-
-
-const products = [
-  { id: 1, name: 'Телефон', price: 500, quantity: 3 },
-  { id: 2, name: 'Ноутбук', price: 1200, quantity: 0 },
-  { id: 3, name: 'Планшет', price: 300, quantity: 5 }
-];
-
-// Используем map для создания нового массива с расширенной информацией о продуктах
-const updatedProducts = products.map((product) => {
-  return {
-      ...product,
-      status: product.quantity > 0 ? 'в наличии' : 'нет в наличии'
-  };
-});
-
-// console.log(updatedProducts);
-
-let numbers1 =[1, 2, 4, 7, 10, 332, 0, 232]
-
-const newArr = numbers1.map((el) => el === 7 ? el = 3 : el)
-
-// console.log(newArr );
+const doubledNumbers = numbers.map((num) => num * 2);
+// console.log(doubledNumbers);
+// 2.
+const names = ["Oleg", "Ekaterina", "Egor", "Ira"]
+// Используем метод map для преобразования каждой строки в длину этой строки
+const lengthOfNames = names.map((name) => name.length);
+// console.log(lengthOfNames); // Вывод: [4, 9, 4, 3]
+// names - исходный массив строк ["Oleg", "Ekaterina", "Egor", "Ira"].
+// map - метод массива, который выполняет функцию обратного вызова для каждого элемента массива.
+// (name) => name.length - коллбэк функция, которая принимает каждый элемент массива names (строку name) и возвращает длину этой строки (name.length).
+// Результат выполнения метода map будет новый массив lengthOfNames, содержащий длины каждой строки из исходного массива names:
+// Длина строки "Oleg" равна 4.
+// Длина строки "Ekaterina" равна 9.
+// Длина строки "Egor" равна 4.
+// Длина строки "Ira" равна 3.
+// 3.
+const auto = ["LADA", "HONDA", "KIA", "AUDI"]
+const stringtoUpperCase = auto.map((name) => name.toLowerCase());
+// console.log(stringtoUpperCase);
+// map - метод массива, который применяет функцию обратного вызова ко всем элементам массива.
+// (name) => name.toLowerCase() - коллбэк функция, которая принимает каждую строку из массива auto (переменная name) и преобразует её в нижний регистр с помощью метода toLowerCase().
+// Результат выполнения метода map будет новый массив stringsToLowerCase, содержащий все строки из исходного массива auto, преобразованные в нижний регистр:
+// "LADA" преобразуется в "lada".
+// "HONDA" преобразуется в "honda".
+// "KIA" преобразуется в "kia".
+// "AUDI" преобразуется в "audi".
+// 4. 
+const numbers1 = [123, 212, 345, 5534, 52345];
+const stringToString = numbers1.map((num) => num.toString())
+// console.log(stringToString);
+// map - метод массива, который применяет функцию обратного вызова ко всем элементам массива.
+// (num) => num.toString() - коллбэк функция, которая принимает каждое число из массива numbers1 (переменная num) и преобразует его в строковое представление с помощью метода toString().
+// Результат выполнения метода map будет новый массив stringNumbers, содержащий строковые представления всех чисел из исходного массива numbers1:
+// 123 преобразуется в "123".
+// 212 преобразуется в "212".
+// 345 преобразуется в "345".
+// 5534 преобразуется в "5534".
+// 52345 преобразуется в "52345".
+// 5.
+const objArray = [{id: 1, date: 22}, {id: 2, date: 56}, {id: 3, date: 48}];
